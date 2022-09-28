d09c3aacdc220a85ee6dac8238cc8990d08b50d3
najmusskb <najmussakib173@gmail.com>
2022-09-28 15:20:35 +0600

    local storage set

--- src/utilities/fakedblocaltrorage.js
+++ src/utilities/fakedblocaltrorage.js
@@ -1,18 +1,33 @@
 // use local storage to mansge cart data
 const addtodatabase=id=>{
-    const quantity = localStorage.getItem(id);
-    if(quantity){
-        console.log("already esxist")
-        const newQuantity=(parseInt(quantity) +1);
-        localStorage.setItem(id,newQuantity);
-    }
+
+    let ShoppingCart;
+    // get the shoping cart 
+    const storedcart =localStorage.getItem('shoping-cart');
+    if(storedcart){
+ShoppingCart=JSON.parse(storedcart)    }
+
     else{
-        console.log('new items')
-        localStorage.setItem(id,1);
+        ShoppingCart={};
+    }
+    // add quantity
+    const quantity = ShoppingCart[id]
+;    if(quantity){
+        // console.log("already esxist")
+        const newQuantity=(parseInt(quantity) + 1);
+        ShoppingCart[id]=newQuantity;
+        // localStorage.setItem(id, newQuantity);
+        
+
     }
 
 
-
+    else{
+        // console.log('new items')
+        // localStorage.setItem(id,1);
+        ShoppingCart[id]=1;
+    }
+    localStorage.setItem('shoping-cart',JSON.stringify(ShoppingCart));
 }
 
 export {addtodatabase}
